06d18f736336e583af825f29a6936b4db92a1744
Alan Williams <alanwill81@gmail.com>
2016-01-03 22:07:45 -0800

    Initial commit

---
+++ shorten-url.py
@@ -0,0 +1,26 @@
+import sys
+
+from oauth2client import client
+from googleapiclient import sample_tools
+
+def main(argv):
+    service, flags = sample_tools.init(
+        argv, 'urlshortener', 'v1', __doc__, __file__,
+        scope='https://www.googleapis.com/auth/urlshortener')
+
+    try:
+        url = service.url()
+
+        longurl = raw_input('Enter the URL to be shortened: ')
+        body = {'longUrl': longurl }
+        resp = url.insert(body=body).execute()
+
+        print ""
+        print "Shortened URL:   " + resp['id']
+        print "Long URL:        " + resp['longUrl']
+
+    except client.AccessTokenRefreshError:
+        print ('The credentials have been revoked or expired, please re-run the application to re-authorize')
+
+if __name__ == '__main__':
+    main(sys.argv)
